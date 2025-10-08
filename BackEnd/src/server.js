4456721aafb9a7974dba2cd01850051c605676ae
Naol Bekele <naolbekele193@gmail.com>
2025-10-08 20:44:25 +0300

    prepared for the deployment

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import path from 'path'
 import notesRouter from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js'
 import rateLimitter from './middleware/rateLimiter.js'
@@ -9,22 +10,34 @@ dotenv.config()
 const app = express()
 
 const PORT = process.env.PORT || 5001
-app.use(cors())
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({ origin: "http://localhost:5173", }))
+}
+
 
 //middleware
 app.use(express.json())
-// app.use((req, res, next) => {
-//     console.log(`Req Method${req.method} & Req.URI is ${req.url}`);
-//     next()
 app.use(rateLimitter)
 
 
 // }) 
 app.use('/api/notes', notesRouter)
 
-connectDB().then(()=>{
-app.listen(PORT, () => {
-    console.log('your server is running on port ', PORT);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../FrontEnd/dist")))
+
+  // ✅ Express 5-compatible catch-all route
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../FrontEnd/dist/index.html"))
+  })
+}
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('your server is running on port ', PORT);
     })
 
 })
